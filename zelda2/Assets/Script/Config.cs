fa51c74c921b7c6a5c7db5491071b840ec9ef1b1
GismoGaming <gismogaming@gmail.com>
2017-07-14 16:09:08 -0400

    asfa
    asdf

---
+++ zelda2/Assets/Script/Config.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Config : MonoBehaviour {
+
+	public static string HORIZONTAL_INPUT = "Horizontal";
+	public static string VERTICAL_INPUT = "Vertical";
+
+}
---
+++ zelda2/Assets/Script/Map_Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map_Movement : MonoBehaviour {
+
+	private string inputAxisH = Config.HORIZONTAL_INPUT;
+	private string inputAxisV = Config.VERTICAL_INPUT;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float inputH = Input.GetAxis (inputAxisH);
+		float inputV = Input.GetAxis (inputAxisV);
+
+
+	}
+}
